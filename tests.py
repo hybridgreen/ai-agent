2fac79961f955ecc9c83d3b477e9d8216127ab7d
hybridgreen <y.yayaoye@gmail.com>
2025-06-18 16:50:21 +0900

    Added the power to write

--- tests.py
+++ tests.py
@@ -1,13 +1,13 @@
-from functions.functions import get_files_info, get_file_content
+from functions.functions import get_files_info, get_file_content , write_file
 
 def test_calculator():
-    result = get_file_content("calculator", "main.py")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print (f'File length: {len(result)} \nFile content:{result}')
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print (f'File length: {len(result)} \nFile content:{result}')
 
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print (f'File length: {len(result)} \nFile content:{result}')    
     pass
 
